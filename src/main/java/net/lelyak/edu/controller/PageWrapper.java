5937c1cfb51c339e1fae53bfb02086b540fb43be
Nazar Lelyak <nazar.lelyak@gmail.com>
2018-01-19 13:06:24 +0200

    add lombok annotations to PageWrapper

--- src/main/java/net/lelyak/edu/controller/PageWrapper.java
+++ src/main/java/net/lelyak/edu/controller/PageWrapper.java
@@ -1,25 +1,21 @@
 package net.lelyak.edu.controller;
 
 import com.google.common.collect.Lists;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
-
+@Getter @Setter
 public class PageWrapper<T> {
     public static final int MAX_PAGE_ITEM_DISPLAY = 5;
+
     private Page<T> page;
     private List<PageItem> items;
     private int currentNumber;
     private String url;
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 
     public PageWrapper(Page<T> page, String url) {
         this.page = page;
@@ -48,10 +44,6 @@ public class PageWrapper<T> {
         for (int i = 0; i < size; i++) {
             items.add(new PageItem(start + i, (start + i) == currentNumber));
         }
-    }
-
-    public List<PageItem> getItems() {
-        return items;
     }
 
     public int getNumber() {
@@ -90,18 +82,11 @@ public class PageWrapper<T> {
         return page.hasNext();
     }
 
+    @Getter
+    @AllArgsConstructor
     public class PageItem {
         private int number;
         private boolean current;
-
-        public PageItem(int number, boolean current) {
-            this.number = number;
-            this.current = current;
-        }
-
-        public int getNumber() {
-            return this.number;
-        }
 
         public boolean isCurrent() {
             return this.current;
