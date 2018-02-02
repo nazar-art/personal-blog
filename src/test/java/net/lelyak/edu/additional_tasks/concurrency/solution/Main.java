69fdbc6b025c937c982d2850dfb50207d6a3c186
Nazar Lelyak <nlelyak@lohika.com>
2018-02-02 17:37:57 +0200

    use interface link instead of child one

--- src/test/java/net/lelyak/edu/additional_tasks/concurrency/solution/Main.java
+++ src/test/java/net/lelyak/edu/additional_tasks/concurrency/solution/Main.java
@@ -2,6 +2,7 @@ package net.lelyak.edu.additional_tasks.concurrency.solution;
 
 import lombok.extern.slf4j.Slf4j;
 import net.lelyak.edu.additional_tasks.concurrency.solution.writer.FileWriter;
+import net.lelyak.edu.additional_tasks.concurrency.solution.writer.Writer;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -25,7 +26,7 @@ public class Main {
 
         ExecutorService executor = Executors.newFixedThreadPool(reportsNumber);
 
-        FileWriter fileWriter = new FileWriter(destination);
+        Writer fileWriter = new FileWriter(destination);
         CallableReportTask reportTask = new CallableReportTask(fileWriter);
 
         for (int i = 0; i < reportsNumber; i++) {
